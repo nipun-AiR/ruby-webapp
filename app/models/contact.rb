0c080364e51c05e6c27c8123d5c07dd99f892e3f
Nipun Iyer <nipunair92@gmail.com>
2017-04-09 01:24:05 +0000

    flash and favicon

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, format: { with: /@/ , message: "not correct format. Please enter valid email"}
+    validates :comments, presence: true
 end
